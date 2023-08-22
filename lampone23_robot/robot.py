da7d752c013a43077e866cda3992f5086322cc0a
Petr Neduchal <neduchal@gmail.com>
2023-08-22 11:21:19 +0200

    Prvni verze firmwaru

---
+++ lampone23_robot/robot.py
@@ -0,0 +1,58 @@
+import rclpy
+from rclpy.node import Node
+
+from jetbot import Robot
+
+import time
+from geometry_msgs.msg import TwistStamped
+
+class LamponeRobot(Node):
+
+    def __init__(self):
+        super().__init__('lampone_robot')
+        self.current_move = [0.0, 0.0]
+        self.last_timestamp= time.time()
+        self.robot_subscriber = self.create_subscription(
+            TwistStamped,
+            'cmd_vel',
+            self.robot_callback,
+            10)  
+        timer_period = 0.1  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.robot = Robot()
+   
+    def robot_callback(self, data):
+            self.current_move[0] = data.twist.linear.x
+            self.current_move[1] = 0.5 * data.twist.angular.z
+            self.last_timestamp = time.time()
+
+    def timer_callback(self):
+        if (time.time() - self.last_timestamp) > 1:
+            self.current_move = [0.0, 0.0]
+        if self.current_move[0] != 0.0:
+            self.robot.set_motors(0.5 * self.current_move[0], 0.5 * self.current_move[0])
+        elif self.current_move[1] != 0.0:
+            if self.current_move[1] > 0:
+                self.robot.right(speed=0.5)
+            elif self.current_move[1] < 0:
+                self.robot.left(speed=0.5)
+        else:
+            self.robot.stop()
+            
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    robot = LamponeRobot()
+
+    rclpy.spin(robot)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    robot.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
